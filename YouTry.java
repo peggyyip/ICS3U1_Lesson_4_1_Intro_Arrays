11caf6aaeede3f81226e7f6ac652297d76a4870e
peggyyip <peggy.yip25@ycdsbk12.ca>
2024-05-06 14:22:41 -0400

    updated

--- YouTry.java
+++ YouTry.java
@@ -1,8 +1,19 @@
 class YouTry extends ConsoleProgram {
   public void run() {
     // Declare and initialize your arrays here
+    int [] canCounts = new int[5];
 
+    canCounts[0] = 4;
+    canCounts[1] = 10;
+    canCounts[2] = 12;
+    canCounts[3] = 18;
+    canCounts[4] = 10;
 
+    System.out.println(canCounts[0]);
+    System.out.println(canCounts[4]);
+
+    String[] top5 = {"You're my sunshine"};
+    
     // Assign values to canCounts here
 
 
